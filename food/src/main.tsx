73922bff41650a6ae94e30da4817288d00b43812
MarceloALMoreira <marceloaomoreira@hotmail.com>
2024-01-26 15:48:26 -0300

    feat: add setup config tailwind and shadcn-ui

--- food/src/main.tsx
+++ food/src/main.tsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h2>Bem vindo</h2>
+    <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
   </React.StrictMode>
 );
